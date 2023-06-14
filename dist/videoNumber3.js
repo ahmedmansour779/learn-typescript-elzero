17d5985de3450e7139a3beebfd695cceffb495f0
ahmedmansour779 <ahmedmansour779>
2023-06-14 11:55:29 +0300

    video number 3

---
+++ dist/videoNumber3.js
@@ -0,0 +1,14 @@
+"use strict";
+let theName = "Elzero";
+let theAge = 40;
+let hire = true;
+let all = "Elzero Web School";
+let allVars;
+theName = "Osama";
+all = 100;
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(10, 20));
+console.log(typeof add(10, 20));
+//# sourceMappingURL=videoNumber3.js.map
