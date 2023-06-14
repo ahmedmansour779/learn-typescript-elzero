7fa02bd8c39f8dacc2d8efd65d16a94f4bd8dda2
ahmedmansour779 <ahmedmansour779>
2023-06-15 01:11:48 +0300

    videos { 5, 6, 7, 8, 9, 10 }

---
+++ dist/videoNumber9.js
@@ -0,0 +1,6 @@
+"use strict";
+function showData(name, age, country = "US") {
+    return `${name} - ${age} - ${country}`;
+}
+console.log(showData("Osama", 40, "Egypt"));
+//# sourceMappingURL=videoNumber9.js.map
